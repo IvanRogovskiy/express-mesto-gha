1fcce0ca08f11c92b22ca416dd38e5500aa3b019
Ivan Rogovskiy <ivanrogovskiy@Ivans-MacBook-Pro-2.local>
2022-06-28 17:34:11 +0300

    fixed eslint

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,4 +1,4 @@
-module.exports = (err, req, res, next) => {
+module.exports = (err, req, res) => {
   const { statusCode = 500, message } = err;
   res.status(statusCode).send({ message: statusCode === 500 ? 'Внутренняя ошибка сервера' : message });
 };
