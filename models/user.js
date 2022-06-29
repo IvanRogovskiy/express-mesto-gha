6f0cddd079503998bc104ed3bb04bf79b2dd1602
Ivan Rogovskiy <ivanrogovskiy@Ivans-MacBook-Pro-2.local>
2022-06-29 13:55:30 +0300

    eslint fixed 2

--- models/user.js
+++ models/user.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const { ValidationError } = require('../errors/ValidationError');
-const { UserNotFound } = require('../errors/UserNotFound');
 const { UnauthorizedError } = require('../errors/UnauthorizedError');
 
 const userSchema = new mongoose.Schema({
